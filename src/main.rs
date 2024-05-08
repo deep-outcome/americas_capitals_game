8c6eed144c3ec6b7df7f7b527aefb82280d46a66
JirCep <uzivatel919@seznam.cz>
2024-05-08 18:18:03 +0200

    dynamic range for countries

--- src/main.rs
+++ src/main.rs
@@ -169,7 +169,7 @@ fn main() -> std::io::Result<()> {
 
     let mut buff = String::with_capacity(2);
     let num = loop {
-        write!(&mut stdout, "Tell batch size [1-34]: ")?;
+        write!(&mut stdout, "Tell batch size [1-{}]: ", COUNTRIES)?;
         stdout.flush()?;
         buff.clear();
 
